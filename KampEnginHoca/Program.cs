a24768d9832e11f3f846091b612e99fb0178f5e4
ozturkyunusemre <ozturkyunusemre511@gmail.com>
2021-03-06 01:01:53 +0300

    6.03.2021

---
+++ KampEnginHoca/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace KampEnginHoca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // type safety  tip güvenliği
+            // Do not repeat yourself kendini tekrarlama
+            // değer tutucu alias
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = false;
+            double dolarDun = 7.45;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Aazalış oku");
+            }
+            else if (dolarBugun>dolarDun)
+            {
+                Console.WriteLine("Artış Oku");
+            }
+            else
+            {
+                Console.WriteLine("değişmedi oku");
+            }
+            if (sistemeGirisYapmisMi==true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giris Yap Butonu");
+            }
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
---
+++ Metotlar/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 85;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            foreach  ( Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("--------------");
+            }
+            Console.WriteLine("---------Metotlar-------");
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+            sepetManager.Ekle2("Armut", 25, "Yeşil Armut");
+            sepetManager.Ekle2("Elma", 20, "Yeşil Elma");
+        }
+    }
+}
+// tekrar tekrar kullanmamızı sağlar
+// dont repeat yourself DRY
+// clean code
+// best preactice 
