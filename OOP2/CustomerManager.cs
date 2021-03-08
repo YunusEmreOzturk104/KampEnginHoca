7cd6d80efc376843a4e80ffa4f6ba0705510badb
ozturkyunusemre <ozturkyunusemre511@gmail.com>
2021-03-08 19:15:47 +0300

    8 Ocak

---
+++ OOP2/CustomerManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    // iş sınıflar  business classes
+    class CustomerManager
+    {
+        public void Add(Musteri musteri)
+    }
+}
---
+++ Oop3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Oop3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtıyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            BasvuruManager basvuruManager = new BasvuruManager();
+
+            IloggerService databaseloggerservice = new DatabaseLoggerService();
+            IloggerService fileloggerservice = new FileLoggerService();
+            basvuruManager.BasvuruYap(ihtiyacKrediManager , new List<IloggerService> { new DatabaseLoggerService(), new SmsLoggerService() });
+            List<IKrediManager> krediler = new List<IKrediManager>() { ihtiyacKrediManager, tasitKrediManager};
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+        }
+    }
+}
